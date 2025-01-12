0caa66edd10687e7690eed78a0128a1c72291527
Bárbara Lourenço Tavares <babisolua@gmail.com>
2025-01-12 09:51:17 -0300

    :sparkles:	feat: add resolução exercicios f e g

---
+++ JavaScript/aula_05/resolucao-exercicio-f.js
@@ -0,0 +1,7 @@
+
+function calcularSoma(num1, num2) {
+    let resultado = num1 + num2
+    console.log(resultado)
+}
+
+calcularSoma(56452, 923415)
---
+++ JavaScript/aula_05/resolucao-exercicio-g.js
@@ -0,0 +1,5 @@
+function apresentarPessoa(nome, idade, profissao) {
+    console.log(`Meu nome é ${nome}, tenho ${idade} anos e sou ${profissao}.`)
+}
+
+apresentarPessoa("Valéria", 44, "cabeleireira")
